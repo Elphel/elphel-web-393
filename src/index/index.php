527e418af1604074a5b7c02a98c1ef0e8aa6915a
Oleg Dzhimiev <oleg@elphel.com>
2017-01-13 15:39:50 -0700

    +license headers

--- src/index/index.php
+++ src/index/index.php
@@ -1,8 +1,58 @@
+<?php
+/**
+ * @file index.php
+ * @brief index page
+ * @copyright Copyright (C) 2017 Elphel Inc.
+ * @author Oleg Dzhimiev <oleg@elphel.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
 <!doctype html>
 <html lang="en">
 <head>
   <title>Elphel 393</title>
   <meta charset="utf-8"/>
+  <script>
+  // for LibreJS:
+  /** 
+  * @file index.php
+  * @copyright Copyright (C) 2017 Elphel Inc.
+  * @author -
+  *
+  * @licstart  The following is the entire license notice for the 
+  *  JavaScript code in this page.
+  *
+  *   The JavaScript code in this page is free software: you can
+  *   redistribute it and/or modify it under the terms of the GNU
+  *   General Public License (GNU GPL) as published by the Free Software
+  *   Foundation, either version 3 of the License, or (at your option)
+  *   any later version.  The code is distributed WITHOUT ANY WARRANTY;
+  *   without even the implied warranty of MERCHANTABILITY or FITNESS
+  *   FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+  *
+  *   As additional permission under GNU GPL version 3 section 7, you
+  *   may distribute non-source (e.g., minimized or compacted) forms of
+  *   that code without the copy of the GNU GPL normally required by
+  *   section 4, provided you include this license notice and a URL
+  *   through which recipients can access the Corresponding Source.
+  *
+  *  @licend  The above is the entire license notice
+  *  for the JavaScript code in this page.
+  */
+  </script>
   <link rel="stylesheet" href="js/bootstrap/css/bootstrap.css">
   <style>
   .port_window{
