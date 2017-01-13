8ce388f5729b79fc4027035dfdfccb6ce16be304
Oleg Dzhimiev <oleg@elphel.com>
2017-01-13 16:04:01 -0700

    license header

--- src/eyesis4pi/eyesis4pi_interface.php
+++ src/eyesis4pi/eyesis4pi_interface.php
@@ -1,23 +1,23 @@
 <?php
-/*
-*!***************************************************************************
-*! FILE NAME  : eyesis4pi_interface.php
-*! DESCRIPTION: command interface for the eyesis4pi gui
-*! Copyright (C) 2016 Elphel, Inc.
-*! --------------------------------------------------------------------------
-*!  This program is free software: you can redistribute it and/or modify
-*!  it under the terms of the GNU General Public License as published by
-*!  the Free Software Foundation, either version 3 of the License, or
-*!  (at your option) any later version.
-*!
-*!  This program is distributed in the hope that it will be useful,
-*!  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*!  GNU General Public License for more details.
-*!
-*!  You should have received a copy of the GNU General Public License
-*!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*! --------------------------------------------------------------------------
+/**
+ * @file eyesis4pi_interface.php
+ * @brief command interface for the eyesis4pi gui
+ * @copyright Copyright (C) 2016 Elphel Inc.
+ * @author Oleg Dzhimiev <oleg@elphel.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 include 'include/elphel_functions_include.php';
